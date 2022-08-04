349b23d5dc5bfeb634e0adbe56ddacb11ae7bfab
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-05 01:35:15 +0900

    [Silver IV] Title: 한수, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int number = sc.nextInt();
+        int number = Integer.parseInt(br.readLine());
         System.out.println(counting(number));
     }
 

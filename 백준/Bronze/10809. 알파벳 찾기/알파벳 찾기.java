62f5368201c5e01d846cb7b2def4c8a21455b58f
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-05 13:50:11 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,17 +1,20 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     static int[] arr = new int[26];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         initialization();
 
-        String S = sc.nextLine();
+        String S = br.readLine();
         indexCheck(S);
+        br.close();
 
         for(int result : arr) {
             sb.append(result).append(" ");

a750ca478992e9001c2309e1e743366253cb78d6
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-06 00:00:47 +0900

    [Bronze I] Title: 분수찾기, Time: 148 ms, Memory: 16316 KB -BaekjoonHub

--- 백준/Bronze/1193. 분수찾기/분수찾기.java
+++ 백준/Bronze/1193. 분수찾기/분수찾기.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int X = sc.nextInt();
+        int X = Integer.parseInt(br.readLine());
         int line = 0;
         int count = 0;
 

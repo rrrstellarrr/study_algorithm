cf8938ea6c6c1965d9acb26c7656441f0b6bf7ed
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-05 15:36:47 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        br.close();
 
         int reverseA = (A / 100) + (((A / 10) % 10) * 10) + ((A % 10) * 100);
         int reverseB = (B / 100) + (((B / 10) % 10) * 10) + ((B % 10) * 100);

20ac8a4a8912ee12bf8f5284ca7b8a9b78f9ce88
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-05 14:55:31 +0900

    [Bronze I] Title: 단어 공부, Time: 304 ms, Memory: 21052 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,10 +1,12 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[26];
-        String str = sc.next().toUpperCase();
+        String str = br.readLine().toUpperCase();
 
         for(int i = 0; i < str.length(); i++) {
             arr[str.charAt(i) - 65]++;

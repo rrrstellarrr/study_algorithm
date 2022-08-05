bbfee9d48470df5e1d3c0d42eb6877b7563b3921
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-05 17:42:11 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 784 ms, Memory: 167704 KB -BaekjoonHub

---
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < T; i++) {
+            String str = br.readLine();
+            int target = str.indexOf(" ");
+            int result = Integer.parseInt(str.substring(0,target)) + Integer.parseInt(str.substring(target + 1));		
+			sb.append(result + "\n");
+        }
+        br.close();
+        System.out.print(sb);
+    }
+}

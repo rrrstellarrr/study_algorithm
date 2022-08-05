1a6f67c9085a9f4c71c44a3b463058a85de5f113
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-05 15:54:04 +0900

    [Bronze II] Title: 다이얼, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,9 +1,11 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
         int count = 0;
 
         for(int i = 0; i < str.length(); i++) {

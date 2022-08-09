b4ea74cdd65b6ce75bf47676a671dc11cea3326d
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-09 23:48:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         int count = 0;
         while(true) {

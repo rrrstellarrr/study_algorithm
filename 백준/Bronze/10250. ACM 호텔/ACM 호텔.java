b675366cf0bc6b04dba04e89453c7c66df6519d6
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-09 14:47:26 +0900

    [Bronze II] Title: ACM 호텔, Time: 128 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int T = sc.nextInt();
-        
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
         for(int i = 0; i < T; i++) {
-            int H = sc.nextInt();
-            int W = sc.nextInt();
-            int N = sc.nextInt();
-            
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int H = Integer.parseInt(st.nextToken());
+            Integer.parseInt(st.nextToken());
+            int N = Integer.parseInt(st.nextToken());
+
             if(N % H == 0) {
                 sb.append((H * 100) + (N / H)).append("\n");
             } else {

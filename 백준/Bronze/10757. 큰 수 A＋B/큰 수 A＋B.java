a4c050d3b2411c859a056d66b87d651d9574897d
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-10 01:28:48 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 244 ms, Memory: 17332 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -1,12 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        BigInteger A = sc.nextBigInteger();
-        BigInteger B = sc.nextBigInteger();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        BigInteger A = new BigInteger(str[0]);
+        BigInteger B = new BigInteger(str[1]);
 
         System.out.println(A.add(B));
     }

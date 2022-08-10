f946d045af747065e224dca6078de6332a78cd5c
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-10 15:58:50 +0900

    [Bronze I] Title: 소인수분해, Time: 128 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,20 +1,24 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
         for(int i = 2; i * i <= N; i++) {
             while(N % i == 0) {
                 N /= i;
-                System.out.println(i);
+                sb.append(i).append("\n");
             }
         }
         if(N != 1) {
-            System.out.println(N);
+            sb.append(N);
         }
+        System.out.print(sb);
     }
 }

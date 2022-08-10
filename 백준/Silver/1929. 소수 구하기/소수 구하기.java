8d5ecdb8b9c8846fff5a2c9415827f6977070137
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-10 17:34:14 +0900

    [Silver III] Title: 소수 구하기, Time: 276 ms, Memory: 20704 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     static boolean[] prime;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         StringBuilder sb = new StringBuilder();
 
-        int M = sc.nextInt();
-        int N = sc.nextInt();
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
 
         prime = new boolean[N + 1];
         isPrime();

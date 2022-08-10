512f8b29383d15920c1b0738b6f95d4f665ae844
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-10 19:18:13 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 124 ms, Memory: 14084 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,11 +1,13 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(br.readLine());
         int[] fibonacci = new int[n + 1];
 
         for(int i = 0; i <= n; i++) {

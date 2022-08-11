99817336b901d49295ce541f0860aff2d97e4db1
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-11 14:37:08 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 136 ms, Memory: 14500 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,34 +1,38 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
 
     static String underscore ="";
+    static StringBuilder sb = new StringBuilder();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
 
-        System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
         chatbot(N);
+        System.out.print(sb);
     }
 
     public static void chatbot(int N) {
         String under = underscore;
 
         if(N == 0) {
-            System.out.println(under + "\"재귀함수가 뭔가요?\"");
-            System.out.println(under + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-            System.out.println(under + "라고 답변하였지.");
+            sb.append(under).append("\"재귀함수가 뭔가요?\"").append("\n");
+            sb.append(under).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+            sb.append(under).append("라고 답변하였지.").append("\n");
             return;
         }
-        System.out.println(under + "\"재귀함수가 뭔가요?\"");
-        System.out.println(under + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-        System.out.println(under + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-        System.out.println(under + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+        sb.append(under).append("\"재귀함수가 뭔가요?\"").append("\n");
+        sb.append(under).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
+        sb.append(under).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
+        sb.append(under).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
 
         underscore += "____";
         chatbot(N - 1);
-        System.out.println(under + "라고 답변하였지.");
+        sb.append(under).append("라고 답변하였지.").append("\n");
     }
 }

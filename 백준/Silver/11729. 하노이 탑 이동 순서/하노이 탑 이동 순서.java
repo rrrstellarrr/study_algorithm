9d77e42ed4206ddb7e75def1016871c29e847c2e
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-11 16:58:48 +0900

    [Silver I] Title: 하노이 탑 이동 순서, Time: 528 ms, Memory: 55040 KB -BaekjoonHub

--- 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
+++ 백준/Silver/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.java
@@ -1,30 +1,32 @@
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.util.Scanner;
 
 public class Main {
 
-    static StringBuilder sb = new StringBuilder();
-    static int count = 0;
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
+        
+        bw.write((int) (Math.pow(2, N) - 1) + "\n");
 
         hanoi(N, 1, 2, 3);
-        System.out.println(count);
-        System.out.print(sb);
+        bw.flush();
+        bw.close();
     }
 
-    private static void hanoi(int n, int start, int mid, int to) {
-        count++;
-
+    private static void hanoi(int n, int start, int mid, int to) throws IOException {
         if(n == 1) {
-            sb.append(start).append(" ").append(to).append("\n");
+            bw.write(start + " " + to + "\n");
             return;
         }
 
         hanoi(n - 1, start, to, mid);
-        sb.append(start).append(" ").append(to).append("\n");
+        bw.write(start + " " + to + "\n");
         hanoi(n - 1, mid, start, to);
     }
 }

ff731542ea53230f70913f5b26da674be9d0e184
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-13 22:23:05 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -1,19 +1,20 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
 
-        int N = sc.nextInt();
-
+        int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][2];
 
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j < 2; j++) {
-                arr[i][j] = sc.nextInt();
-            }
+            st = new StringTokenizer(br.readLine(), " ");
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
         for(int i = 0; i < N; i++) {

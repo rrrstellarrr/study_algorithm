2a7f0f18d4eef8c36468e8e43509bd6bc1f3abc9
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-15 15:31:45 +0900

    [Silver V] Title: 영화감독 숌, Time: 308 ms, Memory: 86012 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int number = 666;
+        int count = 1;
+
+        while(count != N) {
+            number++;
+
+            if(String.valueOf(number).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(number);
+    }
+}

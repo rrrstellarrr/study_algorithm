f7c5e7fcc69ed841afa0d020252fdbb48052121c
Stella Lee <stella.is.a.starrr@gmail.com>
2022-08-17 15:13:14 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,24 +1,21 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class Main {
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        InputStream input = System.in;
         StringBuilder sb = new StringBuilder();
 
-        String N = sc.next();
-        int len = N.length();
-        int[] arr = new int[len];
+        int[] count = new int[10];
 
-        for(int i = 0; i < len; i++) {
-            arr[i] = N.charAt(i) - '0';
+        int num;
+        while((num = input.read()) != '\n') {
+            count[num - '0']++;
         }
 
-        Arrays.sort(arr);
-
-        for(int i = len - 1; i >= 0; i--) {
-            sb.append(arr[i]);
+        for(int i = 9; i >= 0; i--) {
+            while(count[i]-- > 0) sb.append(i);
         }
         System.out.print(sb);
     }
